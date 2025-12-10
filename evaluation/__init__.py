cf043ec2a5e4051a6ce087d48c0e04b0ef0a5af7
Rohan Siva <rohansiva123@gmail.com>
2025-12-10 12:05:24 -0600

    init

---
+++ evaluation/__init__.py
@@ -0,0 +1,41 @@
+
+
+from .metrics import (
+    robust_accuracy,
+    attack_success_rate,
+    average_perturbation_norm,
+    confidence_on_adversarial,
+    robustness_gap,
+    calculate_all_metrics
+)
+from .visualization import (
+    plot_adversarial_examples,
+    plot_attack_comparison,
+    plot_defense_comparison,
+    plot_robustness_curve,
+    plot_transferability_matrix,
+    plot_training_history,
+    plot_confidence_distribution
+)
+from .robustness_evaluation import (
+    RobustnessEvaluator,
+    compare_models
+)
+
+__all__ = [
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    
+]
